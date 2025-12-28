eb196bf71dfebadc470e4749d0418e3fca28c0c1
Tolga BAYRAK <bayraktolga28@gmail.com>
2025-12-28 13:15:46 +0300

    e

--- web/src/pages/home.tsx
+++ web/src/pages/home.tsx
@@ -1,8 +1,7 @@
 export default function Home() {
-  return (
-    <div>
-      <h1>Selam</h1>
-      <button className="btn btn-soft">Default</button>
-    </div>
-  )
+    return (
+        <div>
+            <h1>Home</h1>
+        </div>
+    )
 }
